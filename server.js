ac089b755d8f389c819e0e47fa91608181740938
levimendyk <levi3743@gmail.com>
2022-09-08 15:57:30 -0600

    added process.env.PORT to const port and added a Procfile for Heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const apiroutes = require("./routes/apiroutes");
 const htmlroutes = require("./routes/htmlroutes");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
